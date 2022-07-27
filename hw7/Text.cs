9fb31eea66da5e6ada05520bf508211ca5a3c74d
Ricky <rpadr014@fiu.edu>
2022-07-26 21:21:10 -0400

    added Text class

---
+++ hw7/Text.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW7
+{
+    [Serializable]
+    public class Text
+    {
+        public Text()
+        {
+
+        }
+        public String SavedText { get; set; }
+        public int zOrder { get; set; }
+        public Color Color { get; set; }
+        public Color BackColor { get; set; }
+        public Point Location { get; set; } = new Point(0, 0);
+        public Font Font { get; set; }
+        //public Rotation ?         Not sure what data type rotation will be. Add when available
+    }
+}
